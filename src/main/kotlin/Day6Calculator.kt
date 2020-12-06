97951e8337c79b90d9ce9c6ca80224b4cb219933
Nick <nick_de_clerck@hotmail.com>
2020-12-06 13:12:02 +0100

    Day6

---
+++ src/main/kotlin/Day6Calculator.kt
@@ -0,0 +1,26 @@
+class Day6Calculator : AbstractDayCalculator(6) {
+
+    override fun calculatePart1(): Int {
+        val groups = getInputAsText().split("\r\n\r\n")
+        var answers = 0
+        for (group in groups) {
+            answers += group.replace(Regex("\\s"), "").toSet().size
+        }
+        return answers
+    }
+
+    override fun calculatePart2(): Int {
+        val groups = getInputAsText().split("\r\n\r\n")
+        var answers = 0
+        for (group in groups) {
+            val answersPerPerson = group.split("\n")
+            var uniqueChars = answersPerPerson[0].toSet()
+            for (char in answersPerPerson) {
+                uniqueChars = uniqueChars.intersect(char.toSet())
+            }
+            answers += uniqueChars.size
+        }
+        return answers
+    }
+
+}
